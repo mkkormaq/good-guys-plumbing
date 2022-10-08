07aaced1381b45922a3a580fc1bf65e99a0c3f52
Josh McCormack <joshgmc@gmail.com>
2022-10-07 22:58:13 -0400

    inititialized repo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// fetch('https://api.thecatapi.com/v1/images/search')
+//     .then(res => res.json())
+//     .then(data => {
+//         const img = data[0].url
+//         document.querySelector('#bannerImage').style.backgroundImage = `url(${img})`
+//     })
+//     .catch(err => console.error(err))
